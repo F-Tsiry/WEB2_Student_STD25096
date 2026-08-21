44918a5f205a6a46e1c07715e5a43108f5aaaf91
FANOMEZANTSOA Tsiry Lucien <lucien@mail.hei.school>
2026-08-21 18:57:30 +0300

    fix: adapt index

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { StudentController } from './src/controllers/SudentController';
+import { AuthController } from './src/controllers/AuthController';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +9,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Instanciation des contrôleurs
 new StudentController(app);
+new AuthController(app);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
